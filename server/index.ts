3976bda4b94451e1c0e54b5945575c85b41dbbf0
vincent <vincent.cy@foxmail.com>
2022-07-24 22:40:05 +0800

    wip: 发现新大陆

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,8 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 
 import next from 'next';
 
+import bodyparser from 'koa-bodyparser';
 
 import Koa, { Context, Next } from 'koa';
 
@@ -9,29 +10,66 @@ import Router from '@koa/router';
 
 import { config } from './config';
 
-import { createConnections } from 'typeorm';
+import {
+  createConnections,
+  // getConnectionManager,
+  getTreeRepository,
+} from 'typeorm';
+import { Globaluser } from './model/mmo_account/entities/Globaluser';
 
 const app = next({ dev: config.isDev });
 
-const handle = app.getRequestHandler();
+// const handle = app.getRequestHandler();
 
 app.prepare().then(async () => {
-  const server = new Koa();
-
-  const router = new Router();
-
   await createConnections();
 
-  // router.all('(.*)', async (ctx: Context) => {
-  //   await handle(ctx.req, ctx.res);
-  //   ctx.respond = false;
-  // });
+  const server = new Koa();
+  const router = new Router();
 
-  server.use(async (ctx: Context, next: Next) => {
-    await handle(ctx.req, ctx.res);
-    await next();
+  router.get('/api/hello', async (ctx: Context) => {
+    const url: string = ctx.url,
+      query: object = ctx.query,
+      queryString: string = ctx.querystring;
+    ctx.cookies.set('chen', 'hello');
+    // const account_db = getConnectionManager().get('account')
+    const blogRepository = getTreeRepository(Globaluser, 'account');
+    // console.log('account_db', account_db)
+    console.log('blogRepository', blogRepository);
+    let s = await blogRepository.find();
+    console.log('s', s);
+    ctx.body = {
+      url,
+      query,
+      queryString,
+      s,
+    };
   });
 
+  router.post('/api/login', async (ctx, next: Next) => {
+    const url: string = decodeURIComponent(ctx.url),
+      query: object = ctx.query,
+      queryString: string = ctx.querystring;
+    // @ts-ignore
+    let bodyData = ctx.request.body;
+    console.log('sssss', bodyData);
+    ctx.cookies.set('kssssey', 'sssss');
+    ctx.body = {
+      url,
+      query,
+      queryString,
+      bodyData,
+    };
+    next();
+  });
+
+  // server.use(async (ctx: Context, next: Next) => {
+  // //   await handle(ctx.req, ctx.res);
+  // console.log('ctx',ctx);
+  //   await next();
+  // });
+
+  server.use(bodyparser());
   server.use(router.routes());
   server.listen(config.port, () => {
     console.log(`> Ready on http://localhost:${config.port}`);
--- server/model/mmo_account/entities/Globaluser.ts
+++ server/model/mmo_account/entities/Globaluser.ts
@@ -1,4 +1,4 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 /**
  * globaluser
  * @Date 2022/7/22 15:16:22
@@ -7,111 +7,103 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
  * @class Globaluser
  */
 @Entity({
-    name: 'globaluser'
+  name: 'globaluser',
 })
 export class Globaluser {
-    /**
-     * 主键 id
-     *
-     * @type { number }
-     * @memberof Globaluser
-     */
-    @PrimaryGeneratedColumn()
-    id: number;
-    /**
-     * 用户唯一ID，自增字段
-     *
-     * @type { number }
-     * @memberof Globaluser
-     */
-    @Column('int',{ name: 'userid' })
-    userid: number;
-    /**
-     * 用户帐户的字符串
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'account' })
-    account: string;
-    /**
-     * 玩家的密码
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'passwd' })
-    passwd: string;
-    /**
-     * 玩家的身份证号码
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'identity' })
-    identity: string;
-    /**
-     * 帐号的创建时间
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'createtime' })
-    createtime: string;
-    /**
-     * 上次登录时间
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'updatetime' })
-    updatetime: string;
-    /**
-     * 用户上次登录的IP地址，64位整形IP地址，支持IPV6(现在记录创账号ip)
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'updateip' })
-    updateip: string;
-    /**
-     * 用户离线时间，用于防沉迷判断当前登录距离上次下线是否足5小时
-     *
-     * @type { string }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'offlinetime' })
-    offlinetime: string;
-    /**
-     * 连续在线时间，用于进行防沉迷控制
-     *
-     * @type { number }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'fcmonline' })
-    fcmonline: number;
-    /**
-     * 账号自创建以来的累积在线时长，单位是秒
-     *
-     * @type { number }
-     * @memberof Globaluser
-     */
-    @Column('varchar',{ name: 'totalonline' })
-    totalonline: number;
-    /**
-     * 玩家的GM等级，普通玩家是0.GM等级越高表示权限越高。1-10级GM
-     *
-     * @type { number }
-     * @memberof Globaluser
-     */
-    @Column('int',{ name: 'gmlevel' })
-    gmlevel: number;
-    /**
-     * 玩家在的服务器的index
-     *
-     * @type { number }
-     * @memberof Globaluser
-     */
-    @Column('int',{ name: 'inserver' })
-    inserver: number;
+  /**
+   * 主键 id
+   *
+   * @type { number }
+   * @memberof Globaluser
+   */
+  @PrimaryGeneratedColumn()
+  userid: number;
+  /**
+   * 用户帐户的字符串
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'account' })
+  account: string;
+  /**
+   * 玩家的密码
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'passwd' })
+  passwd: string;
+  /**
+   * 玩家的身份证号码
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'identity' })
+  identity: string;
+  /**
+   * 帐号的创建时间
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'createtime' })
+  createtime: string;
+  /**
+   * 上次登录时间
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'updatetime' })
+  updatetime: string;
+  /**
+   * 用户上次登录的IP地址，64位整形IP地址，支持IPV6(现在记录创账号ip)
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'updateip' })
+  updateip: string;
+  /**
+   * 用户离线时间，用于防沉迷判断当前登录距离上次下线是否足5小时
+   *
+   * @type { string }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'offlinetime' })
+  offlinetime: string;
+  /**
+   * 连续在线时间，用于进行防沉迷控制
+   *
+   * @type { number }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'fcmonline' })
+  fcmonline: number;
+  /**
+   * 账号自创建以来的累积在线时长，单位是秒
+   *
+   * @type { number }
+   * @memberof Globaluser
+   */
+  @Column('varchar', { name: 'totalonline' })
+  totalonline: number;
+  /**
+   * 玩家的GM等级，普通玩家是0.GM等级越高表示权限越高。1-10级GM
+   *
+   * @type { number }
+   * @memberof Globaluser
+   */
+  @Column('int', { name: 'gmlevel' })
+  gmlevel: number;
+  /**
+   * 玩家在的服务器的index
+   *
+   * @type { number }
+   * @memberof Globaluser
+   */
+  @Column('int', { name: 'inserver' })
+  inserver: number;
 }
